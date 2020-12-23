ee47cb65e5b1889506fc98c3d5823e3d47051461
bedo368 <engbedo489@gmail.com>
2020-12-23 16:27:21 +0200

    client

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         const connect = await mongoose.connect(
             // "mongodb://127.0.0.1:27017",
-            process.env.DATA_BASE_URL,
+            `${process.env.DATA_BASE_URL}`,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -4,164 +4,170 @@ import { useDispatch, useSelector } from "react-redux"
 import Message from "../../components/message/Message"
 import Loader from "../../components/loader/Loader"
 import {
-  getUserProfile,
-  updateUserProfile,
+    getUserProfile,
+    updateUserProfile,
 } from "../../redux/user/userProfile/userProfileAction"
 const ProfilePage = ({ location, history }) => {
-  const dispatch = useDispatch()
-  const { userInfo } = useSelector((state) => state.userReducer)
-  const {
-    userProfileInfo,
-    getUserProlieFetchState,
-    userProfileErrorMessage,
-  } = useSelector((state) => state.userProfileReducer)
+    const dispatch = useDispatch()
+    const { userInfo } = useSelector((state) => state.userReducer)
+    const {
+        userProfileInfo,
+        getUserProlieFetchState,
+        userProfileErrorMessage, 
+    } = useSelector((state) => state.userProfileReducer)
 
-  const [editProfile, setEditProfile] = useState(false)
-  const [userInfoInput, setUserInfo] = useState({
-    email: "",
-    newPassword: "",
-    name: "",
-    oldPassword: "",
-  })
-  const { email, newPassword, name, oldPassword } = userInfoInput
-  const redirect = location.search ? location.search.split("=")[1] : "/"
-
-  const userInfoChange = (event) => {
-    const { name, value } = event.target
-    setUserInfo((preval) => {
-      return { ...preval, [name]: value }
+    const [editProfile, setEditProfile] = useState(false)
+    const [userInfoInput, setUserInfo] = useState({
+        email: "",
+        newPassword: "",
+        name: "",
+        oldPassword: "",
     })
-  }
-  useEffect(() => {
-    
-    if (!userProfileInfo?.name) {
-      if (userInfo) {
-        dispatch(getUserProfile())
-      }
-    } else {
-      setUserInfo((preval) => ({
-        ...preval,
-        email: userProfileInfo.email,
-        name: userProfileInfo.name,
-      }))
-    }
-    if(!userInfo){
-      history.push("/")
-    }
-  }, [userInfo, userProfileInfo, dispatch, ProfilePage])
-  useEffect(() => {
-    if (!userInfo) {
-      history.push(redirect)
-    }
-  }, [history, userInfo])
+    const { email, newPassword, name, oldPassword } = userInfoInput
+    const redirect = location.search ? location.search.split("=")[1] : "/"
 
-  const submitHandler = (event) => {
-    event.preventDefault()
-    dispatch(updateUserProfile(name, email, newPassword, oldPassword))
-    setEditProfile(false)
-  }
-  return (
-    <div>
-      {editProfile ? (
-        <Row>
-          <Col md={6}>
-          
-            <h2> User Profile </h2>
-            {userProfileErrorMessage ? (
-              <Message variant="danger">{userProfileErrorMessage} </Message>
+    const userInfoChange = (event) => {
+        const { name, value } = event.target
+        setUserInfo((preval) => {
+            return { ...preval, [name]: value }
+        })
+    }
+    useEffect(() => {
+        if (!userProfileInfo?.name) {
+            if (userInfo) {
+                dispatch(getUserProfile())
+            }
+        } else {
+            setUserInfo((preval) => ({
+                ...preval,
+                email: userProfileInfo.email,
+                name: userProfileInfo.name,
+            }))
+        }
+        if (!userInfo) {
+            history.push("/")
+        }
+    }, [userInfo, userProfileInfo, dispatch, ProfilePage])
+    useEffect(() => {
+        if (!userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo])
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        dispatch(updateUserProfile(name, email, newPassword, oldPassword))
+        setUserInfo({ email: "", newPassword: "", name: "", oldPassword: "" })
+        setEditProfile(false)
+    }
+    return (
+        <div>
+            {editProfile ? (
+                <Row>
+                    <Col md={6}>
+                        <h2> User Profile </h2>
+                        {userProfileErrorMessage ? (
+                            <Message variant="danger">
+                                {userProfileErrorMessage}{" "}
+                            </Message>
+                        ) : (
+                            userProfileErrorMessage && (
+                                <Message variant="danger">
+                                    {userProfileErrorMessage}{" "}
+                                </Message>
+                            )
+                        )}
+                        {getUserProlieFetchState && <Loader />}
+                        <Form onSubmit={submitHandler}>
+                            <Form.Group controlId="name">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control
+                                    type="name"
+                                    placeholder="Enter name"
+                                    name="name"
+                                    value={name}
+                                    onChange={userInfoChange}
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="email">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Enter email"
+                                    name="email"
+                                    value={email}
+                                    onChange={userInfoChange}
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="newPassword">
+                                <Form.Label>newPassword</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Enter newPassword"
+                                    name="newPassword"
+                                    value={newPassword}
+                                    onChange={userInfoChange}
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="oldPassword">
+                                <Form.Label>oldPassword</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Enter oldPassword"
+                                    name="oldPassword"
+                                    value={oldPassword}
+                                    onChange={userInfoChange}
+                                    required
+                                />
+                            </Form.Group>
+                            <Button type="submit"> update</Button>
+                        </Form>
+                    </Col>
+                </Row>
             ) : (
-              userProfileErrorMessage && (
-                <Message variant="danger">{userProfileErrorMessage} </Message>
-              )
+                <Row>
+                    <Col md={6} className="pt-5">
+                        <h2> User Profile </h2>
+                        {userProfileErrorMessage ? (
+                            <Message variant="danger">
+                                {userProfileErrorMessage}{" "}
+                            </Message>
+                        ) : (
+                            userProfileErrorMessage && (
+                                <Message variant="danger">
+                                    {userProfileErrorMessage}{" "}
+                                </Message>
+                            )
+                        )}
+
+                        {getUserProlieFetchState && <Loader />}
+                        <div className="pt-5">
+                            <h4>
+                                {" "}
+                                <strong>Name:</strong> {name}{" "}
+                            </h4>
+
+                            <h4>
+                                {" "}
+                                <strong>Email:</strong> {email}{" "}
+                            </h4>
+
+                            <Button
+                                className="mt-2"
+                                type="button"
+                                onClick={() => {
+                                    setEditProfile(true)
+                                }}
+                            >
+                                {" "}
+                                EDIT
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
             )}
-            {getUserProlieFetchState && <Loader />}
-            <Form onSubmit={submitHandler}>
-              <Form.Group controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="name"
-                  placeholder="Enter name"
-                  name="name"
-                  value={name}
-                  onChange={userInfoChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="email">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  name="email"
-                  value={email}
-                  onChange={userInfoChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="newPassword">
-                <Form.Label>newPassword</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter newPassword"
-                  name="newPassword"
-                  value={newPassword}
-                  onChange={userInfoChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="oldPassword">
-                <Form.Label>oldPassword</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter oldPassword"
-                  name="oldPassword"
-                  value={oldPassword}
-                  onChange={userInfoChange}
-                  required
-                />
-              </Form.Group>
-              <Button type="submit"> update</Button>
-            </Form>
-          </Col>
-        </Row>
-      ) : (
-        <Row>
-          <Col md={6} className="pt-5">
-           
-            <h2> User Profile </h2>
-            {userProfileErrorMessage ? (
-              <Message variant="danger">{userProfileErrorMessage} </Message>
-            ) : (
-              userProfileErrorMessage && (
-                <Message variant="danger">{userProfileErrorMessage} </Message>
-              )
-            )}
-
-            {getUserProlieFetchState && <Loader />}
-            <div className="pt-5">
-              <h4>
-                {" "}
-                <strong>Name:</strong> {name}{" "}
-              </h4>
-
-              <h4>
-                {" "}
-                <strong>Email:</strong> {email}{" "}
-              </h4>
-
-              <Button
-                className="mt-2"
-                type="button"
-                onClick={() => {
-                  setEditProfile(true)
-                }}
-              >
-                {" "}
-                EDIT
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      )}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default ProfilePage
