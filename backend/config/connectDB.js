52724c15526e17da487c31c842f4f9b178298900
bedo368 <engbedo489@gmail.com>
2021-01-09 20:38:09 +0200

    to deployment

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose"
 const connectDB = async () => {
     try {
         const connect = await mongoose.connect(
-            // "mongodb://127.0.0.1:27017",
             `${process.env.DATA_BASE_URL}`,
             {
                 useNewUrlParser: true,
