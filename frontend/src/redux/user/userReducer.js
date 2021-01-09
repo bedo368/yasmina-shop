f84daf3b5398f8ed07026e17e53c1915e5182fd9
bedo368 <engbedo489@gmail.com>
2021-01-09 21:56:37 +0200

    env don't push

--- frontend/src/redux/user/userReducer.js
+++ frontend/src/redux/user/userReducer.js
@@ -2,7 +2,7 @@ import userTypes from "./userTypes"
 
 const userInfo = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
-  : {token:null}
+  : {token:""}
 const INTIAL_STATE = {
   userInfo: userInfo,
   fetchState: false,
