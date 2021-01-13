2f1bab2377ca9b5c03afa4281888b56f3f9f4a33
bedo <you@example.com>
2021-01-13 21:09:46 +0200

    use post insted of use in graphql

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/upload", uploadRoute)
 dotenv.config()
 const __dirname = path.resolve()
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
-app.use(
+app.post(
     "/graphql",
     graphqlHTTP({
         graphiql: false,
